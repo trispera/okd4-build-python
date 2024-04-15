962888c00a3f0a2822917ad55d121c6331fb1b77
Tristan <tristan.perard@outlook.com>
2024-04-15 14:35:56 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import subprocess
+import smtplib
+import os
+from email.message import EmailMessage
+
+EMAIL_RECIPIENT = os.environ['EMAIL_RECIPIENT']
+OUTPUTFILE = "hello.txt"
+
+with open(OUTPUTFILE, "w") as file:
+    file.write("Hello World from trispera GitHub repo!")
+
+msg = EmailMessage()
+msg["From"] = "noreply@csc.fi"
+msg["Subject"] = "Broken links report"
+msg["To"] = os.gentenv('EMAIL_RECIPIENT')
+msg.set_content("You will find attached the broken links report of docs.csc.fi")
+msg.add_attachment(open(OUTPUTFILE, "r", encoding="utf-8").read(), subtype='txt', filename='hello.txt')
+
+s = smtplib.SMTP('smtp.pouta.csc.fi')
+s.send_message(msg)
+print("Email sent")
